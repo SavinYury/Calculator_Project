bcd66d4adc85e37a68bd4c814a6c5700f5d44e97
Пользователь <savinyura@mail.ru>
2024-11-06 15:21:39 +0700

    Создали первоначальный код программы КАЛЬКУЛЯТОР

---
+++ 2024_11_01_ЛЕКЦИЯ_Calculator_project.py
@@ -0,0 +1,12 @@
+# ПРОГРАММА КАЛЬКУЛЯТОР
+from tkinter import * # Импортируем из tkinter все функции
+from tkinter import ttk # Расширенный вариант tkinter
+
+window = Tk()
+window.title("КАЛЬКУЛЯТОР") # Переименовываем окно window
+window.iconbitmap(default="calculator-icon.ico") # Перезаписываем иконку вместо перышка
+
+
+
+
+window.mainloop()
