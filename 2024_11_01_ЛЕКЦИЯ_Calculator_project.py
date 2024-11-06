687ff88dd60436ff7156ce504c9422fcb0d92072
Пользователь <savinyura@mail.ru>
2024-11-06 15:40:00 +0700

    Добавили главное окно ВЫВОДА информации

--- 2024_11_01_ЛЕКЦИЯ_Calculator_project.py
+++ 2024_11_01_ЛЕКЦИЯ_Calculator_project.py
@@ -6,7 +6,9 @@ window = Tk()
 window.title("КАЛЬКУЛЯТОР") # Переименовываем окно window
 window.iconbitmap(default="calculator-icon.ico") # Перезаписываем иконку вместо перышка
 
-
+### Создаем главное окно ВЫВОДА информации
+line_input = Entry(window, font=("Arial",14)) # Поле для ВЫВОДА визуальной информации
+line_input.grid(row=0,column=0, columnspan=4, sticky="ew") # Устанавливаем его в НУЛЕВОЙ ряд НУЛЕВОЙ столбец и растягиваем на ЧЕТЫРЕ колонки
 
 
 window.mainloop()
